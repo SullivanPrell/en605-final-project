cb5b6d1c7ca03a5c350a315bb8ccdec11d7cdba7
Sullivan Prellwitz <22063556+SullivanPrell@users.noreply.github.com>
2024-04-04 18:29:43 -0500

    this might work

--- test/lib/build.rs
+++ test/lib/build.rs
@@ -3,21 +3,9 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let mut root_proj_dir = env::current_dir().unwrap();
     let current_dir = env::current_dir().unwrap();
-    root_proj_dir.pop();
-    root_proj_dir.pop();
-    let math_lib_asm = root_proj_dir.join("libMath.s");
-    // let rsa_lib_asm = root_proj_dir.join("rsaLib.s");
-
-
-    // Directory where the output object file will be saved
-    let out_dir = root_proj_dir.join("bin");
-    let math_lib_object = out_dir.join("libMath.o");
-    // let rsa_lib_asm = out_dir.join("rsaLib.o");
 
     // Tell cargo to link the static library `libMath`
-    println!("cargo:rustc-link-lib=static=customMathLib");    // Specify the search directory for `libMath.a`
-    println!("cargo:rustc-link-search=native={}", current_dir.join("customMathLib.a").to_str().unwrap());
-
+    println!("cargo:rustc-link-search=native=./");
+    println!("cargo:rustc-link-lib=dylib=customMathLib");
 }
