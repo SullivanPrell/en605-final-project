f73d8934bf2c6bd6ce4e779741ad006ba94fb362
Sullivan Prellwitz <22063556+SullivanPrell@users.noreply.github.com>
2024-04-04 16:28:29 -0500

    Test

---
+++ test/lib/build.rs
@@ -0,0 +1,35 @@
+use std::process::Command;
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let mut root_proj_dir = env::current_dir().unwrap();
+    root_proj_dir.pop();
+    root_proj_dir.pop();
+
+    let math_lib_asm = root_proj_dir.join("mathLib.s");
+    let rsa_lib_asm = root_proj_dir.join("rsaLib.s");
+
+
+    // Directory where the output object file will be saved
+    let out_dir = root_proj_dir.join("/bin/");
+    let math_lib_object = out_dir.join("mathLib.o");
+    let rsa_lib_asm = out_dir.join("rsaLib.o");
+
+
+    // Assemble the ARM assembly file into an object file
+    let status = Command::new("make")
+        .status()
+        .unwrap();
+
+    if !status.success() {
+        panic!("Failed to assemble ARM assembly file");
+    }
+
+    // Tell cargo to tell rustc to link the assembled object file
+    println!("cargo:rustc-link-search=native={}", out_dir.to_str().unwrap());
+    println!("cargo:rustc-link-lib=static=function");
+
+    // Tell cargo to rerun this script if the assembly file changes
+    println!("cargo:rerun-if-changed=function.s");
+}
---
+++ test/lib/src/lib.rs
@@ -0,0 +1,30 @@
+extern "C" {
+    fn totient(p: i32, q: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn totient_pq_prime() {
+        unsafe {
+            let result = totient(73, 97);
+            assert_eq!(result, 6984)
+        }
+    }
+
+    #[test]
+    fn totient_pq_not_prime() {
+        unsafe {
+            let result = totient(72, 96);
+            assert_eq!(result, -1)
+        }
+    }
+}
