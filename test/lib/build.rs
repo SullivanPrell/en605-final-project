987836a3f072313835fa3ac7b5927e5005891b21
Sullivan Prellwitz <22063556+SullivanPrell@users.noreply.github.com>
2024-04-11 12:28:46 -0500

    fix rust i think

--- test/lib/build.rs
+++ test/lib/build.rs
@@ -1,11 +1,9 @@
-use std::process::Command;
 use std::env;
-use std::path::PathBuf;
 
 fn main() {
     let current_dir = env::current_dir().unwrap();
 
     // Tell cargo to link the static library `libMath`
-    println!("cargo:rustc-link-search=native=/home/sprell/en605-final-project/test/lib");
-    println!("cargo:rustc-link-lib=lMath");
+    println!("cargo:rustc-link-search=native={}",current_dir.display());
+    println!("cargo:rustc-link-lib=Math");
 }
--- test/lib/src/lib.rs
+++ test/lib/src/lib.rs
@@ -2,23 +2,28 @@ use libloading::{Library, Symbol};
 
 #[cfg(test)]
 mod tests {
+    use std::env;
     use super::*;
 
     #[test]
     fn totient_pq_prime() {
         unsafe {
-            let libMath = Library::new("libMath.so").unwrap();
-            let totient = libMath.get::<Symbol<extern "C" fn(i32,i32) -> i32>>(b"totient").unwrap();
+            let path = env::current_dir().unwrap();
+            println!("The current directory is {}", path.display());
+            let lib_math = Library::new("./libMath.so").unwrap();
+            let totient = lib_math.get::<Symbol<extern "C" fn(i32, i32) -> i32>>(b"totient").unwrap();
             let result = totient(73, 97);
-            assert_eq!(result, 6984)
+            assert_eq!(result, 6984);
         }
     }
 
     #[test]
     fn totient_pq_not_prime() {
         unsafe {
+            let lib_math = Library::new("./libMath.so").unwrap();
+            let totient = lib_math.get::<Symbol<extern "C" fn(i32, i32) -> i32>>(b"totient").unwrap();
             let result = totient(72, 96);
-            assert_eq!(result, -1)
+            assert_eq!(result, -1);
         }
     }
 }
