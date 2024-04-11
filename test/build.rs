ae3376a972c0259aaf680fa482cf999d48d10197
Sullivan Prellwitz <22063556+SullivanPrell@users.noreply.github.com>
2024-04-11 14:53:00 -0500

    Add comments

--- test/build.rs
+++ test/build.rs
@@ -1,9 +1,9 @@
 use std::env;
 
 fn main() {
-    let current_dir = env::current_dir().unwrap();
+    let current_dir = env::current_dir().unwrap(); // get current directory
 
-    // Tell cargo to link the static library `libMath`
-    println!("cargo:rustc-link-search=native={}",current_dir.display());
-    println!("cargo:rustc-link-lib=Math");
+    // Tell cargo to link the shared library `libMath.so`
+    println!("cargo:rustc-link-search=native={}",current_dir.display()); // .display() == .ToString()
+    println!("cargo:rustc-link-lib=Math"); // Omit the `lib` as rustc will append this to `Math`
 }
--- test/src/lib.rs
+++ test/src/lib.rs
@@ -5,13 +5,24 @@ mod tests {
     use std::env;
     use super::*;
 
+    /* 
+    * General strategy for calling 32bit ARM functions in Rust:
+    * Rust uses snake case for variable names so our math lib is `lib_math` not `libMath`
+    * In the case of integers we will use i32 or a signed 32bit integer value
+    * Rust has very strict typing so mapping ARM to Rust is essential
+    * Expect easy use of r0-r3 for parameters, outside of this other steps must be taken
+    * r0 is the default return register
+    */
+
     #[test]
     fn gcd_expect_true() {
-        unsafe {
-            let lib_math = Library::new("./libMath.so").unwrap();
-            let gcd = lib_math.get::<Symbol<extern "C" fn(i32, i32) -> i32>>(b"gcd").unwrap();
-            let result = gcd(46, 23);
-            let result1 = gcd(450, 125);
+        unsafe { // this is marked unsafe since we are calling functions external to rust
+            let lib_math = Library::new("./libMath.so").unwrap(); // Get the library
+            let gcd = lib_math.get::<Symbol<extern "C" fn(i32, i32) -> i32>>(b"gcd").unwrap(); // get specific function
+            let result = gcd(46, 23); // call the function
+            let result1 = gcd(450, 125); // call the function
+             // assert that the result is equivalent to the vlaue on the right 
+             // the ! is related to error handling NOT negating the results
             assert_eq!(result, 23);
             assert_eq!(result1, 25);
         }
